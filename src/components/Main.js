6dcb5710a9c5525b7b9ce284b0d67e60d7ef952a
Faith Makinde <faithmakinde0@gmail.com>
2023-02-02 10:39:12 +0100

    work completed

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,12 +3,12 @@ import React from 'react'
 export default function Main(props){
     return (
         <div className='main--section--list'>
-            <img src={props.journal.imageUrl} alt='' />
-            <div>
-                <span><h4>{props.journal.location}</h4><a href=''>View on Google Maps</a></span>
-                <h1>{props.journal.title}</h1>
-                <span><p>{props.journal.startDate}</p><p>{props.journal.endDate}</p></span>
-                <p>{props.journal.description}</p>
+            <img src={props.journal.imageUrl} className='travel-img' alt='' />
+            <div className='main-text-section'>
+                <div className='main-text-flex'><h4 className='location'>{props.journal.location}</h4><a href={props.journal.googleMapsUrl} target='_blank' className='map-link' >View on Google Maps</a></div>
+                <h1 className='main-bold'>{props.journal.title}</h1>
+                <span className='date-text'><p>{props.journal.startDate}</p><p>{props.journal.endDate}</p></span>
+                <p className='main-description'>{props.journal.description}</p>
             </div>
         </div>
     )
--- src/components/data.js
+++ src/components/data.js
@@ -7,25 +7,25 @@ export default [
         location: "Japan",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
         startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
+        endDate: " -24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         imageUrl: imgOne
     },
     {
         title: "Sydney Opera House",
         location: "AUSTRALIA",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://www.google.com/maps/place/Australia/@-26.1772288,133.4170119,4z/data=!4m6!3m5!1s0x2b2bfd076787c5df:0x538267a1955b1352!8m2!3d-25.274398!4d133.775136!16zL20vMGNoZ2h5",
         startDate: "27 May, 2021",
-        endDate: " - 8 Jun, 2021",
+        endDate: "-8 Jun, 2021",
         description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
         imageUrl: imgTwo
     },
     {
         title: "Geirangerfjord",
         location: "NORWAY",
-        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        googleMapsUrl: "https://www.google.com/maps/place/Geirangerfjord/@62.1048867,6.7950438,10z/data=!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi",
         startDate: "01 Oct, 2021",
-        endDate: " - 18 Nov, 2021",
+        endDate: "-18 Nov, 2021",
         description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
         imageUrl: imgThree
     }
